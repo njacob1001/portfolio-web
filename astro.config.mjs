048662f09a748517eec375827f6dddce00d899fe
Jacob Gonzalez <v-jacobgonzalez@expediagroup.com>
2023-01-29 15:58:09 -0500

    feat: build

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://njacob1001.github.io',
+  site: 'https://jacobgonzalez.dev',
   base: '/portfolio-web',
 })
