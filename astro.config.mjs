31a0bd67b5192b63a4fce356eb8344302598e23c
Jacob Gonzalez <v-jacobgonzalez@expediagroup.com>
2023-01-29 16:13:22 -0500

    fix: base project

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import tailwind from '@astrojs/tailwind'
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://jacobgonzalez.dev',
-  base: '/portfolio-web',
+  base: '/',
 })
