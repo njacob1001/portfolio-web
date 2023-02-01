b505a38f4a97a550ec4845d70bef15defe88b082
Jacob Gonzalez <v-jacobgonzalez@expediagroup.com>
2023-01-31 22:07:56 -0500

    feat: skills section

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import { defineConfig } from 'astro/config'
 // https://astro.build/config
 import tailwind from '@astrojs/tailwind'
 
+// https://astro-i18next.yassinedoghri.com/
+import astroI18next from 'astro-i18next'
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), astroI18next()],
   site: 'https://jacobgonzalez.dev',
   base: '/',
 })
