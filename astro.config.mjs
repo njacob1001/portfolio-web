a6cf7e29ad02afd502ed7d65456d33d98fdd88db
Jacob Gonzalez <v-jacobgonzalez@expediagroup.com>
2023-02-07 23:35:06 -0500

    feat: use custom css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
-import tailwind from '@astrojs/tailwind'
-
 // https://astro-i18next.yassinedoghri.com/
 import astroI18next from 'astro-i18next'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroI18next()],
+  integrations: [astroI18next()],
   site: 'https://jacobgonzalez.dev',
   base: '/',
 })
