0ba0f9d0b485f76ca82dce057100fc20664d5909
Jacob Gonzalez <v-jacobgonzalez@expediagroup.com>
2023-02-24 17:11:15 -0500

    fix: images path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
 
 // https://astro-i18next.yassinedoghri.com/
-import astroI18next from 'astro-i18next'
+import astroI18next from 'astro-i18next';
+
+// https://astro.build/config
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [astroI18next()],
+  integrations: [astroI18next(), image()],
   site: 'https://jacobgonzalez.dev',
-  base: '/',
-})
+  base: '/'
+});
