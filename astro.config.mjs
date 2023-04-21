9885c4c8046d7e43e6cb15dd41ce9a3da91ed46a
njacob1001 <jacob.gonzalez.co@gmail.com>
2023-04-20 20:28:08 -0500

    feat: add google analitucs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,24 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import partytown from '@astrojs/partytown'
 
 // https://astro-i18next.yassinedoghri.com/
-import astroI18next from 'astro-i18next';
+import astroI18next from 'astro-i18next'
 
 // https://astro.build/config
-import image from "@astrojs/image";
+import image from '@astrojs/image'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [astroI18next(), image()],
+  integrations: [
+    astroI18next(),
+    image(),
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
   site: 'https://jacobgonzalez.dev',
-  base: '/'
-});
+  base: '/',
+})
